63ccb5f09a7ccb10ad2219c73c27ead3d2a2ec7b
David McNelis <davemcnelis@gmail.com>
2021-12-15 21:32:34 -0500

    Update

--- envm.cr
+++ envm.cr
@@ -13,35 +13,35 @@ parser = OptionParser.new do |parser|
     puts "version #{version}"
     exit
   end
-  
-	parser.on "-h", "--help", "Show help" do
-		puts parser
-		exit
+
+  parser.on "-h", "--help", "Show help" do
+    puts parser
+    exit
   end
 
-	parser.on("list", "List .env versions") do
+  parser.on("list", "List .env versions") do
     list = true
     parser.banner = "Usage: envm list"
   end
 
-	parser.on("current", "Show current .env version") do
+  parser.on("current", "Show current .env version") do
     current = true
     parser.banner = "Usage: envm current"
   end
 
-	parser.on("use", "Use a .env version") do
+  parser.on("use", "Use a .env version") do
     use = true
-		if (ARGV.size != 2)
-			STDERR.puts "ERROR: invalid argument."
-			STDERR.puts parser
-			exit(1)
-		end
-		env = ARGV[1]
+    if (ARGV.size != 2)
+      STDERR.puts "ERROR: invalid argument."
+      STDERR.puts parser
+      exit(1)
+    end
+    env = ARGV[1]
 
     parser.banner = "Usage: envm use <env>"
   end
-	
-	parser.missing_option do |option_flag|
+
+  parser.missing_option do |option_flag|
     STDERR.puts "ERROR: #{option_flag} is missing something."
     STDERR.puts ""
     STDERR.puts parser
@@ -58,7 +58,7 @@ end
 parser.parse
 
 if list
-	list_envs
+  list_envs
 elsif current
   current_env
 elsif use
@@ -69,125 +69,125 @@ else
 end
 
 def list_envs
-	STDOUT.puts list.sort.join("\n")
+  STDOUT.puts list.sort.join("\n")
 end
 
 def current_env
-	current = id(".env")
-	if current
-		STDOUT.puts current
-	else
-		STDERR.puts "current version unknown!"
-	end
+  current = id(".env")
+  if current
+    STDOUT.puts current
+  else
+    STDERR.puts "current version unknown!"
+  end
 end
 
 def use_env(env)
-	if list.includes?(env)
-		backup(".env")
-		use(env)
-		STDOUT.puts "using #{env}"
-	else
-		STDERR.puts "#{env} version not found!"  
-	end
+  if list.includes?(env)
+    backup(".env")
+    use(env)
+    STDOUT.puts "using #{env}"
+  else
+    STDERR.puts "#{env} version not found!"
+  end
 end
 
 def list
-	Dir.glob(".env.*", match_hidden: true).map { |line| "#{line.gsub(".env.", "")}" }
+  Dir.glob(".env.*", match_hidden: true).map { |line| "#{line.gsub(".env.", "")}" }
 end
 
 def load(file)
-	if File.exists?(file)
-		File.read(file).split("\n")
-	else
-		[] of String
-	end
+  if File.exists?(file)
+    File.read(file).split("\n")
+  else
+    [] of String
+  end
 end
 
 def envar?(line)
-	str = line.strip()
-	str.match(/.*\=.*/)
+  str = line.strip
+  str.match(/.*\=.*/)
 end
 
 def comment?(line)
-	str = line.strip()
-	str.starts_with?("#")
+  str = line.strip
+  str.starts_with?("#")
 end
 
 def blank?(line)
-	str = line.strip()
-	str === ""
+  str = line.strip
+  str === ""
 end
 
 def parse(line)
-	str = line.strip()
-	if /(.*)\=(.*)/ =~ str
-		[$1, $2]
-	else
-		[nil, nil]
-	end
+  str = line.strip
+  if /(.*)\=(.*)/ =~ str
+    [$1, $2]
+  else
+    [nil, nil]
+  end
 end
 
 def merge(candidate_line, content)
-	if envar?(candidate_line)
-		candidate_envar, candidate_value = parse(candidate_line)
-		found = false
-		content.each_with_index do |content_line, index|
-			if envar?(content_line)
-				content_envar, content_value = parse(content_line)
-				if candidate_envar === content_envar
-					found = true
-					content[index] = "#{candidate_envar}=#{candidate_value}"
-				end
-			end
-		end
-		if !found
-			content.push("#{candidate_envar}=#{candidate_value}")
-		end
-	elsif blank?(candidate_line) || comment?(candidate_line)
-		content.push(candidate_line)
-	end
+  if envar?(candidate_line)
+    candidate_envar, candidate_value = parse(candidate_line)
+    found = false
+    content.each_with_index do |content_line, index|
+      if envar?(content_line)
+        content_envar, content_value = parse(content_line)
+        if candidate_envar === content_envar
+          found = true
+          content[index] = "#{candidate_envar}=#{candidate_value}"
+        end
+      end
+    end
+    if !found
+      content.push("#{candidate_envar}=#{candidate_value}")
+    end
+  elsif blank?(candidate_line) || comment?(candidate_line)
+    content.push(candidate_line)
+  end
 end
 
 def loadPartials(env)
-	files = Dir.glob(".env.#{env}*" , match_hidden: true).sort
-	base_files = files.select { |file| file === ".env.#{env}" }
-	base_file = if base_files.size === 1
-	 base_files.pop
-	end
-	partial_files = files.reject { |file| file === ".env.#{env}" }
-	content = if base_file 
-		load(base_file)
-	end || [] of String
-	partial_files.each do |partial_file|
-		partial = load(partial_file)
-		partial.each do |line|
-			merge(line, content)
-		end
-	end
-	content
+  files = Dir.glob(".env.#{env}*", match_hidden: true).sort
+  base_files = files.select { |file| file === ".env.#{env}" }
+  base_file = if base_files.size === 1
+                base_files.pop
+              end
+  partial_files = files.reject { |file| file === ".env.#{env}" }
+  content = if base_file
+              load(base_file)
+            end || [] of String
+  partial_files.each do |partial_file|
+    partial = load(partial_file)
+    partial.each do |line|
+      merge(line, content)
+    end
+  end
+  content
 end
 
 def save(lines, file)
-	File.write(file, lines.join("\n"))
+  File.write(file, lines.join("\n"))
 end
 
 def strip(lines)
-	(lines || [] of String).reject { |line| line.starts_with?("ENVM=") } 
+  (lines || [] of String).reject { |line| line.starts_with?("ENVM=") }
 end
 
 def backup(file)
-	if File.exists?(file)
-		save(load(file), "#{file}~")
-	end
+  if File.exists?(file)
+    save(load(file), "#{file}~")
+  end
 end
 
 def use(env)
-	save(strip(loadPartials(env)).unshift("ENVM=#{env}\n"), ".env")
+  save(strip(loadPartials(env)).unshift("ENVM=#{env}\n"), ".env")
 end
 
 def id(file)
-	ids = load(file).select { |line| line.starts_with?("ENVM=") }.map { |line| line.gsub("ENVM=", "") }
-	if ids.size === 1
-		ids.pop
-	end
+  ids = load(file).select { |line| line.starts_with?("ENVM=") }.map { |line| line.gsub("ENVM=", "") }
+  if ids.size === 1
+    ids.pop
+  end
 end
